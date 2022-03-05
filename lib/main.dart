49955e37b67b20836c3df9ad04571560a52cad0c
Aashita <aashita920@gmail.com>
2022-03-05 11:43:21 +0530

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter App',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter App'),
+        ),
+        body: Column(
+          children: <Widget>[
+            Card(
+              child: Text('CHART!'),
+            ),
+            Card(
+              child: Text('LIST OF TX!!!'),
+            )
+          ],
+        ));
+  }
+}
