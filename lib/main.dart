69925149edb8768fdce565f2444c2b738174ba26
Aashita <aashita920@gmail.com>
2022-03-05 11:57:02 +0530

    Updates

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: prefer_const_constructors
 
+import './transaction.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -17,23 +18,31 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key}) : super(key: key);
-
+  final List<Transaction> transactions = [];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Flutter App'),
-        ),
-        body: Column(
-          children: <Widget>[
-            Card(
+      appBar: AppBar(
+        title: Text('Flutter App'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            child: Card(
+              color: Colors.blue,
               child: Text('CHART!'),
+              elevation: 5,
             ),
-            Card(
-              child: Text('LIST OF TX!!!'),
-            )
-          ],
-        ));
+          ),
+          Card(
+            color: Colors.red,
+            child: Text('LIST OF TX!!!'),
+          )
+        ],
+      ),
+    );
   }
 }
